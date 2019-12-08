f0e7600b5db95171ee69b59a784562faf79eb261
Kevin Elliott <kevin@welikeinc.com>
2019-12-08 02:09:00 -0800

    Broadcast active flights

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventsModule } from '../events/events.module';
-import { EventsGateway } from '../events/events.gateway';
+// import { EventsGateway } from '../events/events.gateway';
+import { Flight } from '../entities/flight.entity';
 import { ScheduleService } from './schedule.service';
 import { Station } from '../entities/station.entity';
 
 @Module({
   imports: [
     EventsModule,
+    TypeOrmModule.forFeature([Flight]),
     TypeOrmModule.forFeature([Station])
   ],
   providers: [ScheduleService],
--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -4,32 +4,34 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { EventsGateway } from '../events/events.gateway';
+import { Flight } from '../entities/flight.entity';
 import { Station } from '../entities/station.entity';
 
 @Injectable() // Only support SINGLETON scope
-export class ScheduleService extends NestSchedule {  
+export class ScheduleService extends NestSchedule {
   constructor(
     @Inject("EventsGateway") private readonly eventsGateway: EventsGateway,
+    @InjectRepository(Flight) private readonly flightRepository: Repository<Station>,
     @InjectRepository(Station) private readonly stationRepository: Repository<Station>,
   ) {
     super()
   }
 
   private readonly logger = new Logger(ScheduleService.name);
-  
+
   // @Cron('0 0 2 * *', {
-  //   startTime: new Date(), 
+  //   startTime: new Date(),
   //   endTime: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
   // })
   // async cronJob() {
   //   console.log('executing cron job');
   // }
-  
+
   // @Timeout(5000)
   // onceJob() {
   //   console.log('executing once job');
   // }
-  
+
   @Interval(3000)
   async intervalJob() {
     this.logger.log('Job running to gather and broadcast updated data.');
@@ -38,6 +40,12 @@ export class ScheduleService extends NestSchedule {
     let stations = await this.stationRepository.find();
     this.logger.log('Preparing to broadcast ' + stations.length + ' stations.');
     this.eventsGateway.broadcast('stations', stations)
+
+    let activeFlights = await this.flightRepository.find({
+      where: { status: 'in-flight' }
+    });
+    this.logger.log('Preparing to broadcast ' + activeFlights.length + ' active flights.');
+    this.eventsGateway.broadcast('activeFlights', activeFlights);
 
     // if you want to cancel the job, you should return true;
     // return true;
