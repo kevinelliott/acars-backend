22cf57023fa05eedb52c2631df4c1ddc96d5f449
Kevin Elliott <kevin@welikeinc.com>
2020-02-06 16:51:20 -0800

    Testing out an idea

--- src/entities/station.entity.ts
+++ src/entities/station.entity.ts
@@ -4,7 +4,7 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
-  JoinTable,
+  JoinColumn,
   PrimaryGeneratedColumn,
   OneToMany,
   OneToOne,
@@ -32,6 +32,7 @@ export class Station {
   messages: [];
 
   @OneToOne(type => Station, station => station.id)
+  @JoinColumn()
   stationMessageCount: StationMessageCount;
 
   public messagesCount?: number;
