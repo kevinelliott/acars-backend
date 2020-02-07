d41336146f5a4fcad86e53bdc2b4577054f985e3
Kevin Elliott <kevin@welikeinc.com>
2020-02-06 16:15:46 -0800

    Testing out an idea

--- src/entities/station_message_count.entity.ts
+++ src/entities/station_message_count.entity.ts
@@ -2,12 +2,14 @@ import {
   Entity,
   Column,
   OneToOne,
+  PrimaryColumn,
 } from 'typeorm';
 
 import { Station } from './station.entity';
 
 @Entity('stations')
 export class StationMessageCount {
+  @PrimaryColumn()
   @OneToOne(type => Station, station => station.id)
   stationId: number;
 
