db7ce612ce422fc7b28d83ad71775810298b59c1
Kevin Elliott <kevin@welikeinc.com>
2020-10-06 11:50:55 -0700

    Use port 80 and disable SSL for now

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,18 @@ async function bootstrap() {
   const fs = require('fs');
   let app;
 
-  if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod') {
-    const keyFile  = fs.readFileSync('/certs/api.airframes.io-key.pem');
-    const certFile = fs.readFileSync('/certs/api.airframes.io-cert.pem');
-    app = await NestFactory.create(AppModule, {
-      httpsOptions: {
-        key: keyFile,
-        cert: certFile,
-      }
-    });
-  } else {
-    app = await NestFactory.create(AppModule, {});
-  }
+  // if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod') {
+  //   const keyFile  = fs.readFileSync('/certs/api.airframes.io-key.pem');
+  //   const certFile = fs.readFileSync('/certs/api.airframes.io-cert.pem');
+  //   app = await NestFactory.create(AppModule, {
+  //     httpsOptions: {
+  //       key: keyFile,
+  //       cert: certFile,
+  //     }
+  //   });
+  // } else {
+  app = await NestFactory.create(AppModule, {});
+  // }
 
   const eventsMicroservice = app.connectMicroservice({
     transport: Transport.TCP,
@@ -47,7 +47,7 @@ async function bootstrap() {
   app.enableCors(corsOptions);
 
   if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod') {
-    await app.listen(443);
+    await app.listen(80);
   } else {
     await app.listen(3001);
   }
